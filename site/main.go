bde212793168d24d2acddde02c049cf53e1de2e8
Georg <75206704+Georg97@users.noreply.github.com>
2024-08-09 23:20:40 +0200

    updated main.go

--- site/main.go
+++ site/main.go
@@ -14,6 +14,9 @@ func main() {
     component := hikersHello("Georg")
 
     http.Handle("/", templ.Handler(component))
+    http.HandleFunc("/test", func (w http.ResponseWriter, req *http.Request) {
+        fmt.Fprintf(w, "This is the test")
+    })
 
     fmt.Printf("Listening on :%v\n", env.Port)
     http.ListenAndServe(fmt.Sprintf(":%d", env.Port), nil)
