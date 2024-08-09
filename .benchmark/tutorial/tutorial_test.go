235b703c835c64ebf2bbd872df4c571990dc27f8
Georg <75206704+Georg97@users.noreply.github.com>
2024-08-09 20:55:31 +0200

    added a more sophisticated benchmark to tutorial with a writeToBuffer benchmark

--- .benchmark/tutorial/tutorial_test.go
+++ .benchmark/tutorial/tutorial_test.go
@@ -1,11 +1,31 @@
 package main
 
 import (
+	"bytes"
 	"testing"
 )
 
 // Questions
-// what does b.N do?
+// what does b.N do? -> It is created by the go benchmark utility and runs the code until go thinks its ok now (until differences are not so big anymore)
+
+func writeBuffer(msg []byte) {
+    buf := new(bytes.Buffer)
+    buf.Write(msg)
+}
+
+func BenchmarkBuffer(b *testing.B) {
+    n := 100
+
+    b.Run("rawdog bytes-array", func(b *testing.B) {
+        msg := []byte("Hello, World")
+        for i := 0; i < b.N; i++ {
+            for i := 0; i < n; i++ {
+                writeBuffer(msg)
+            }
+        }
+    })
+}
+
 
 func BenchmarkSlice(b *testing.B) {
     n := 100
