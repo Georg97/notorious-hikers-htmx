86e0fea1c9d7a91f6d19004e20049ef07e118c93
Georg <75206704+Georg97@users.noreply.github.com>
2024-08-09 20:30:40 +0200

    added first benchmark to benchmark

--- .benchmark/tutorial/tutorial_test.go
+++ .benchmark/tutorial/tutorial_test.go
@@ -1,9 +1,28 @@
 package main
 
 import (
-    "testing"
+	"testing"
 )
 
-func Test1(t *testing.T) {
-    // t.Fatalf("Hello? %s\n", "No!")
+// Questions
+// what does b.N do?
+
+func BenchmarkNoAlloc(b *testing.B) {
+    n := 100
+    // outside of bench
+    for i := 0; i < b.N; i++ {
+        // bench code
+        // fmt.Printf("Hello, World")
+        ints := []int{}
+        for i := 0; i < n; i++ {
+            ints = append(ints, i)
+        }
+    }
 }
+
+// func BenchmarkAlloc(b *testing.B) {
+//     // outside of bench
+//     for i := 0; i < b.N; i++ {
+//         // bench code
+//     }
+// }
