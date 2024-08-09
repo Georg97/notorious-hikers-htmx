62eb8a8a3fd0c32808bbf9649f544ec332aad033
Georg <75206704+Georg97@users.noreply.github.com>
2024-08-09 20:46:33 +0200

    refactored benchmark a bit

--- .benchmark/tutorial/tutorial_test.go
+++ .benchmark/tutorial/tutorial_test.go
@@ -7,28 +7,31 @@ import (
 // Questions
 // what does b.N do?
 
-func BenchmarkAlloc(b *testing.B) {
+func BenchmarkSlice(b *testing.B) {
     n := 100
-    // outside of bench
-    for i := 0; i < b.N; i++ {
-        // bench code
-        // fmt.Printf("Hello, World")
-        ints := []int{}
-        for i := 0; i < n; i++ {
-            ints = append(ints, i)
+
+    b.Run("slice non alloc", func(b *testing.B) {
+        // outside of bench
+        for i := 0; i < b.N; i++ {
+            // bench code
+            // fmt.Printf("Hello, World")
+            ints := []int{}
+            for i := 0; i < n; i++ {
+                ints = append(ints, i)
+            }
         }
-    }
+    })
+
+    b.Run("slice alloc", func(b *testing.B) {
+        // outside of bench
+        for i := 0; i < b.N; i++ {
+            // bench code
+            // fmt.Printf("Hello, World")
+            ints := make([]int, n)
+            for i := 0; i < n; i++ {
+                ints[i] = i
+            }
+        }
+    })
 }
 
-func BenchmarkNoAlloc(b *testing.B) {
-    n := 100
-    // outside of bench
-    for i := 0; i < b.N; i++ {
-        // bench code
-        // fmt.Printf("Hello, World")
-        ints := make([]int, n)
-        for i := 0; i < n; i++ {
-            ints[i] = i
-        }
-    }
-}
